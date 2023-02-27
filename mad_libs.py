d3023315ff97c3e7a13e678e252ad0ea2a95c982
MikRub84 <125875052+MikRub84@users.noreply.github.com>
2023-02-27 13:52:20 +0400

    Update mad_libs.py

--- mad_libs.py
+++ mad_libs.py
@@ -17,7 +17,14 @@ if n == 1:
     word11 = input('Enter Adjective of choice and press enter:')
     word12 = input('Enter Silly Word, press space, Noun of choice and press enter:')
     
-    story1 = "It was about " + word1 + " ago when I arrived at the hospital in a/an " + word2 + ". The hospital is a/an " + word3 + " place, there are a lot of " + word4 + " here. There are nurses here, who have " + word5 + ". If someone wants to come into my room, I told them, that they have to " + word6 + " first. I've decorated my room with " + word7 + ". Today I talked to a doctor and they were wearing a/an " + word8 + " on their " + word9 + ". I heard, that all doctors " + word10 + " every day for breakfast. The most " + word11 + " thing about being in the hospital is the " + word12 + "!"
+    story1 = "\t It was about " + word1 + " ago when I arrived at the hospital in a/an " + word2 + ". " + 
+"\n The hospital is a/an " + word3 + " place, there are a lot of " + word4 + " here." + 
+"\n There are nurses here, who have " + word5 + "." + 
+"\n If someone wants to come into my room, I told them, that they have to " + word6 + " first." + 
+"\n I've decorated my room with " + word7 + "." + 
+"\n Today I talked to a doctor and they were wearing a/an " + word8 + " on their " + word9 + "." + 
+"\n I heard, that all doctors " + word10 + " every day for breakfast." + 
+"\n The most " + word11 + " thing about being in the hospital is the " + word12 + "!"
     print('\n')
     print(story1)
     
@@ -37,7 +44,15 @@ if n == 2:
     temp13 = input("Enter Number, press space, Silly Word of choice and press enter:")
     temp14 = input("Enter Noun of choice and press enter:")
     
-    story2 = "This weekend I am going camping with " + temp1 + ". I packed my lantern, sleeping bag and " + temp2 + ". I am so " + temp3 + " to " + temp4 + " in a tent. I am " + temp5 + " we might see a/an " + temp6 + ", I hear they're kind of dangerouse. While we're camping, we are going to hike, fish and " + temp7 + ". I have heard, that the " + temp8 + " lake is great for " + temp9 + ". Then we will " + temp10 +" hike through the forest for " + temp11 + ". If I see a/an " + temp12 + " while hiking, I am going to bring it home as a pet! At night we will tell " + temp13 + " stories and roast " + temp14 + " around the campfire!!"
+    story2 = "\t This weekend I am going camping with " + temp1 + "." + 
+"\n I packed my lantern, sleeping bag and " + temp2 + "." + 
+"\n I am so " + temp3 + " to " + temp4 + " in a tent." + 
+"\n I am " + temp5 + " we might see a/an " + temp6 + ", I hear they're kind of dangerouse." + 
+"\n While we're camping, we are going to hike, fish and " + temp7 + "." + 
+"\n I have heard, that the " + temp8 + " lake is great for " + temp9 + "." + 
+"\n Then we will " + temp10 +" hike through the forest for " + temp11 + "." + 
+"\n If I see a/an " + temp12 + " while hiking, I am going to bring it home as a pet!" + 
+"\n At night we will tell " + temp13 + " stories and roast " + temp14 + " around the campfire!!"
     print('\n')
     print(story2)
     
@@ -57,6 +72,12 @@ if n == 3:
     text13 = input("Enter Verb(ending in ing) of choice and press enter:")
     text14 = input("Enter Adjective, press space, Noun of choice and press enter:")
     
-    story3 = "Dear " + text1 + ", I am writing to you from a/an " + text2 + " castle in an enchanted forest. I found myself here one day after going for a ride on a/an " + text3 + " in " + text4 + ". There are " + text5 + " and " + text6 + " here! In the " + text7 + " there is a pool full of " + text8 + ". I fall asleep each night on a/an " + text9 + " of " + text10 + " and dream of " + text11 + ". It feels as though I have lived here for " + text12 + ". I hope one day you can visit, although the only way to get here now is " + text13 + " on a/an " + text14 + "!!"
+    story3 = "\t Dear " + text1 + ", I am writing to you from a/an " + text2 + " castle in an enchanted forest." + 
+"\n I found myself here one day after going for a ride on a/an " + text3 + " in " + text4 + "." + 
+"\n There are " + text5 + " and " + text6 + " here!" + 
+"\n In the " + text7 + " there is a pool full of " + text8 + "." + 
+"\n I fall asleep each night on a/an " + text9 + " of " + text10 + " and dream of " + text11 + "." + 
+"\n It feels as though I have lived here for " + text12 + "." + 
+"\n I hope one day you can visit, although the only way to get here now is " + text13 + " on a/an " + text14 + "!!"
     print('\n')
     print(story3)
